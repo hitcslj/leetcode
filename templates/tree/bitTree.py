6c4e8945fd301e39de23d13284fd8bc04fae7fca
hitcslj <hitcslj@stu.hit.edu.cn>
2023-11-17 10:57:30 +0800

    bitTreemax

--- templates/tree/bitTree.py
+++ templates/tree/bitTree.py
@@ -42,4 +42,45 @@ class NumArray:
 # obj = NumArray(nums)
 # obj.update(index,val)
 # param_2 = obj.sumRange(left,right)
- 
+
+
+# https://leetcode.cn/problems/maximum-sum-queries/
+from bisect import bisect_left
+
+class BinaryIndexedTree:
+    __slots__ = ["n", "c"]
+
+    def __init__(self, n: int):
+        self.n = n
+        self.c = [-1] * (n + 1)
+
+    def update(self, x: int, v: int):
+        while x <= self.n:
+            self.c[x] = max(self.c[x], v)
+            x += x & -x
+
+    def query(self, x: int) -> int:
+        mx = -1
+        while x:
+            mx = max(mx, self.c[x])
+            x -= x & -x
+        return mx
+
+
+class Solution:
+    def maximumSumQueries(self, nums1: List[int], nums2: List[int], queries: List[List[int]]) -> List[int]:
+        nums = sorted(zip(nums1,nums2),key=lambda x:-x[0])
+        nums2.sort()
+        n,m = len(nums1),len(queries)
+        ans = [-1]*m
+        j = 0
+        tree = BinaryIndexedTree(n)
+        for i in sorted(range(m),key=lambda i:-queries[i][0]):
+            x,y = queries[i]
+            while j<n and nums[j][0]>=x:
+                k = n - bisect_left(nums2,nums[j][1])
+                tree.update(k,nums[j][0]+nums[j][1])
+                j += 1
+            k = n - bisect_left(nums2,y)
+            ans[i] = tree.query(k)
+        return ans
