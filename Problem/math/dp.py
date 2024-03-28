f6974b082235ed6d26f85cfea7b34ee3705103c4
hitcslj <hitcslj@stu.hit.edu.cn>
2024-03-28 11:31:19 +0800

    dp

---
+++ Problem/math/dp.py
@@ -0,0 +1,12 @@
+from typing import List
+
+# https://leetcode.cn/problems/first-day-where-you-have-been-in-all-the-rooms
+
+class Solution:
+    def firstDayBeenInAllRooms(self, nextVisit: List[int]) -> int:
+        n = len(nextVisit)
+        f = [0] * n
+        mod = 10**9 + 7
+        for i in range(1,n):
+            f[i] = (f[i-1] + 1 + f[i-1] - f[nextVisit[i-1]]+1)%mod
+        return f[-1]
