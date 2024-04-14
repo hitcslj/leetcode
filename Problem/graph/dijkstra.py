1ac0da65bc6f0467dea9815645395b8824304971
hitcslj <hitcslj@stu.hit.edu.cn>
2024-04-14 21:29:04 +0800

    dijkstar

--- Problem/graph/dijkstra.py
+++ Problem/graph/dijkstra.py
@@ -1,5 +1,32 @@
 from typing import List
 from math import inf
+from heapq import heappop, heappush
+
+
+# https://leetcode.cn/problems/network-delay-time/solutions/2668220/liang-chong-dijkstra-xie-fa-fu-ti-dan-py-ooe8/
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        g = [[] for _ in range(n)]  # 邻接表
+        for x, y, d in times:
+            g[x - 1].append((y - 1, d))
+
+        dis = [inf] * n
+        dis[k - 1] = 0
+        h = [(0, k - 1)]
+        while h:
+            dx, x = heappop(h)
+            if dx > dis[x]:  # x 之前出堆过
+                continue
+            for y, d in g[x]:
+                new_dis = dx + d
+                if new_dis < dis[y]:
+                    dis[y] = new_dis  # 更新 x 的邻居的最短路
+                    heappush(h, (new_dis, y))
+        mx = max(dis)
+        return mx if mx < inf else -1
+
+ 
+
 
 # https://leetcode.cn/problems/design-graph-with-shortest-path-calculator
 
@@ -40,3 +67,28 @@ class Graph:
 # obj = Graph(n, edges)
 # obj.addEdge(edge)
 # param_2 = obj.shortestPath(start,end)
+
+
+
+
+
+# https://leetcode.cn/problems/minimum-time-to-visit-disappearing-nodes/
+class Solution:
+    def minimumTime(self, n: int, edges: List[List[int]], disappear: List[int]) -> List[int]:
+        ans = [-1] * n
+        ans[0] = 0
+        
+        path = [[] for _ in range(n)]
+        for u, v, w in edges:
+            path[u].append((v, w))
+            path[v].append((u, w))
+        
+        q = [(0, 0)]
+        while q:
+            d, u = heappop(q)
+            if ans[u] != d: continue
+            for v, w in path[u]:
+                if (ans[v] == -1 or d + w < ans[v]) and d + w < disappear[v]:
+                    ans[v] = d + w
+                    heappush(q, (ans[v], v))
+        return ans
