401935cfd63f9baab745e604472030d04b28f263
hitcslj <hitcslj@stu.hit.edu.cn>
2025-01-05 06:27:50 +0800

    segTree, lazy note

--- Problem/old/tree/segTree.py
+++ Problem/old/tree/segTree.py
@@ -83,3 +83,79 @@ class RangeModule:
 # param_2 = obj.queryRange(left,right)
 # obj.removeRange(left,right)
  
+
+ class Node:
+    def __init__(self, l, r):
+        self.left = None
+        self.right = None
+        self.l = l
+        self.r = r
+        self.mid = (l+r)>>1
+        self.v = 0
+        self.add = 0
+
+class SegmentTree:
+    def __init__(self):
+        self.root = Node(0, 10**9)
+    
+    def modify(self, l:int, r:int, v:int, node: Node = None):
+        if l > r:
+            return 
+        if node is None:
+            node = self.root
+        if node.l >= l and node.r <= r:
+            node.v += v
+            node.add += v
+            return 
+        self.pushdown(node)
+        if l <= node.mid:
+            self.modify(l,r,v,node.left)
+        if r > node.mid:
+            self.modify(l,r,v,node.right)
+        self.pushup(node)
+
+    def query(self, l: int, r: int, node: Node = None):
+        if l > r:
+            return 0
+        if node is None:
+            node = self.root
+        if node.l >= l and node.r <= r:
+            return node.v
+        self.pushdown(node)
+        v = 0
+        if l <= node.mid:
+            v = max(v, self.query(l,r,node.left))
+        if r > node.mid:
+            v = max(v, self.query(l,r,node.right))
+        return v
+
+
+
+    def pushup(self, node: None):
+        node.v = max(node.left.v, node.right.v)
+
+    def pushdown(self, node: Node):
+        if node.left is None:
+            node.left = Node(node.l, node.mid)
+        if node.right is None:
+            node.right = Node(node.mid + 1, node.r)
+        if node.add:
+            node.left.v += node.add
+            node.right.v += node.add
+            node.left.add += node.add
+            node.right.add += node.add
+            node.add = 0
+
+class MyCalendarThree:
+    def __init__(self):
+        self.tree = SegmentTree()
+    
+    def book(self, startTime: int, endTime: int) -> int:
+        self.tree.modify(startTime,endTime-1,1)
+        return self.tree.query(0,10**9)
+        
+
+
+# Your MyCalendarThree object will be instantiated and called as such:
+# obj = MyCalendarThree()
+# param_1 = obj.book(startTime,endTime)
